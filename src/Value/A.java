1546b139eb2c6570a38702f74f4df8b4ebc8cbfc
Ton-ni <tanyangtong@163.com>
2019-05-09 17:51:59 +0800

    delete unuseful

---
+++ src/Value/A.java
@@ -0,0 +1,18 @@
+package Value;
+
+/**
+ * @author:Administrator
+ * @date: 2019/5/9
+ * @description:
+ */
+public class A {
+    public static void main(String[] args) {
+        Integer a = 1000;
+        Integer b = 1000;
+        Integer c = 100;
+        Integer d = 100;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+
+}
